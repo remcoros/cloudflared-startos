2b6ac42568e68ffab8af615ecc74d7e156fa7516
Remco Ros <remcoros@live.nl>
2025-02-28 12:19:25 +0000

    update to cloudflared 2025.2.1

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "2025.2.0" );
+    .fromMapping({}, "2025.2.1" );
