853e58d4e6fda29e7d1538c54600bc6e64febec3
Remco Ros <remcoros@live.nl>
2025-05-25 11:23:31 +0000

    make it work, add healthcheck and metrics endpoint

--- startos/init/index.ts
+++ startos/init/index.ts
@@ -1,22 +1,14 @@
 import { sdk } from '../sdk'
+import { setInterfaces } from '../interfaces'
 import { versionGraph } from '../install/versionGraph'
 import { actions } from '../actions'
 import { restoreInit } from '../backups'
-import { setToken } from '../actions/setToken'
-
-const setupPostInstall = sdk.setupOnInit(async (effects, kind) => {
-  if (kind == 'install') {
-    // require the config action to run once, to have a password for the ui set
-    await sdk.action.createOwnTask(effects, setToken, 'critical', {
-      reason: 'Configure default settings',
-    })
-  }
-})
 
 export const init = sdk.setupInit(
   restoreInit,
   versionGraph,
-  actions,
+  setInterfaces,
+  actions
 )
 
 export const uninit = sdk.setupUninit(versionGraph)
