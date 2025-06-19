8b0c34b15ae761b787e9dbcaa5149295aba515ba
Remco Ros <remcoros@live.nl>
2025-06-19 16:27:38 +0000

    cloudflared 2025.6.1

--- startos/install/versions/v2025.6.1.ts
+++ startos/install/versions/v2025.6.1.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v2025_6_0 = VersionInfo.of({
-  version: '2025.6.0:1.0',
+export const v2025_6_1 = VersionInfo.of({
+  version: '2025.6.1:1.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
