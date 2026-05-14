c705bc40e59db2f3c94770659de051688db1281d
Remco Ros <remcoros@live.nl>
2026-05-14 18:52:36 +0200

    chore: release 2026.3.0:2

--- startos/versions/v2026.3.0.ts
+++ startos/versions/v2026.3.0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v2026_3_0 = VersionInfo.of({
-  version: '2026.3.0:2-beta.2',
+  version: '2026.3.0:2',
   releaseNotes: {
     en_US:
       'Cloudflare login and tunnel selection, managed public hostnames via the URL plugin, multi-zone DNS support, import of existing routes, and a Managed Public Routes overview action',
