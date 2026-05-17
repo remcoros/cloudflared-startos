498512d613dff12c7d7d1f15569866559f62d127
Remco Ros <remcoros@live.nl>
2026-05-17 13:08:00 +0200

    chore: update cloudflared to 2026.5.0

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,9 +1,10 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v2026_3_0 } from './v2026.3.0'
+import { v2026_5_0 } from './v2026.5.0'
 
 export const versionGraph = VersionGraph.of({
-  current: v2026_3_0,
-  other: [],
+  current: v2026_5_0,
+  other: [v2026_3_0],
 })
 
-export const CLOUDFLARED_VERSION = '2026.3.0'
+export const CLOUDFLARED_VERSION = '2026.5.0'
